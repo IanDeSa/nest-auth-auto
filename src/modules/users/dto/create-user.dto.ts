6a01411b1895df594f2d113a22e897f2351cfe5c
Ian Ecodots <ian@ecodots.com.br>
2023-12-10 00:15:06 -0400

    chore: instalando atualizacoes

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,13 +1,23 @@
 // src/app/modules/user/dto/create-user.dto.ts
 import { ApiProperty } from '@nestjs/swagger';
 import { Permission } from '../enum/enum-permission';
+import { IsEmail, IsString } from 'class-validator';
 
 export class CreateUserDto {
   @ApiProperty({ example: 'john_doe', description: 'Nome do usuário' })
+  @IsString()
   username: string;
 
   @ApiProperty({ example: 'password123', description: 'Senha do usuário' })
+  @IsString()
   password: string;
+
+  @ApiProperty({
+    example: 'john@example.com',
+    description: 'E-mail do usuário',
+  })
+  @IsEmail()
+  email: string;
 
   @ApiProperty({
     enum: Permission,
